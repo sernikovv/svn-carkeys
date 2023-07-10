4551921c248ca091955a36c437081c34e9cb3f06
Sernikov <94058192+sernikovv@users.noreply.github.com>
2023-07-10 17:31:03 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,65 +1,206 @@
 ESX = exports["es_extended"]:getSharedObject()
 
-local ped = PlayerPedId()
+--Otwieranie / zamykanie
 
-RegisterNetEvent('svn-carkeys:zamknijotworz', function()
-    local coords = GetEntityCoords(PlayerPedId())
-    --local vehicle = GetPlayersLastVehicle()
-    local czyzamkniete = GetVehicleDoorLockStatus(vehicle)
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
 
-	if IsPedInAnyVehicle(playerPed, false) then
-		vehicle = GetVehiclePedIsIn(playerPed, false)
+		if IsControlJustReleased(0, 303) and IsInputDisabled(0) then
+			local grracz = PlayerPedId()
+			local kordy = GetEntityCoords(grracz)
+
+			if IsPedInAnyVehicle(grracz, false) then
+				furagracza = GetVehiclePedIsIn(grracz, false)
+			else
+				furagracza = GetClosestVehicle(kordy, 8.0, 0, 71)
+			end
+			local plate = GetVehicleNumberPlateText(furagracza)
+			TriggerServerEvent('svn-checkkeys', plate)
+			Citizen.Wait(100)
+		end
+	end
+end)
+
+RegisterNetEvent('svn-carkeys:otworzamknij')
+AddEventHandler('svn-carkeys:otworzamknij', function()
+	local gracz = PlayerPedId()
+	local kordy = GetEntityCoords(gracz)
+	local czyzamkniete = GetVehicleDoorLockStatus(furagracza)
+
+	if IsPedInAnyVehicle(gracz, false) then
+		furagracza = GetVehiclePedIsIn(gracz, false)
 	else
-		vehicle = GetClosestVehicle(coords, 8.0, 0, 71)
+		furagracza = GetClosestVehicle(kordy, 8.0, 0, 71)
 	end
 
-    if not DoesEntityExist(vehicle) then
-        return
-    end
-
-	if not DoesEntityExist(vehicle) then
+	if not DoesEntityExist(furagracza) then
 		return
 	end
 			if czyzamkniete <= 2 then
-				SetVehicleDoorsLocked(vehicle, 4)
-				SetVehicleDoorsLockedForAllPlayers(vehicle, 1)
-				SetVehicleDoorShut(vehicle, 0, false)
-				SetVehicleDoorShut(vehicle, 1, false)
-				SetVehicleDoorShut(vehicle, 2, false)
-				SetVehicleDoorShut(vehicle, 3, false)
-				SetVehicleDoorShut(vehicle, 4, false)
-				SetVehicleDoorShut(vehicle, 5, false)
-				PlayVehicleDoorCloseSound(vehicle, 1)
-				TriggerEvent('esx:showNotification', 'Pojazd Zamknięty')
-				StartVehicleHorn(vehicle, 100, 0, false)
+				SetVehicleDoorsLocked(furagracza, 4)
+				SetVehicleDoorsLockedForAllPlayers(furagracza, 1)
+				SetVehicleDoorShut(furagracza, 0, false)
+				SetVehicleDoorShut(furagracza, 1, false)
+				SetVehicleDoorShut(furagracza, 2, false)
+				SetVehicleDoorShut(furagracza, 3, false)
+				SetVehicleDoorShut(furagracza, 4, false)
+				SetVehicleDoorShut(furagracza, 5, false)
+				PlayVehicleDoorCloseSound(furagracza, 1)
+				TaskPlayAnim(PlayerPedId(), "gestures@m@standing@casual", "gesture_you_soft", 3.0, 1.0, -1, 48, 0, 0, 0, 0)
+				ESX.ShowNotification('Pojazd Zamknięty')
+				StartVehicleHorn(furagracza, 100, 0, false)
 			elseif czyzamkniete > 2 then
-				SetVehicleDoorsLocked(vehicle, 1)
-				SetVehicleDoorsLockedForAllPlayers(vehicle, false)
-				PlayVehicleDoorOpenSound(vehicle, 0)
-				TriggerEvent('esx:showNotification', 'Pojazd Otwarty')
-				StartVehicleHorn(vehicle, 100, 0, false)
+				SetVehicleDoorsLocked(furagracza, 1)
+				SetVehicleDoorsLockedForAllPlayers(furagracza, false)
+				PlayVehicleDoorOpenSound(furagracza, 0)
+				TaskPlayAnim(PlayerPedId(), "gestures@m@standing@casual", "gesture_you_soft", 3.0, 1.0, -1, 48, 0, 0, 0, 0)
+				ESX.ShowNotification('Pojazd Otwarty')
+				StartVehicleHorn(furagracza, 100, 0, false)
 			end
 end)
 
-local silnikKey = 246 -- Y
+-- Szukanie kluczy
+local searchingKeys = false
+local searchedVehicles = {}
 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
+        local playerPed = PlayerPedId()
+        local vehicle = GetVehiclePedIsIn(playerPed, false)
 
-        if IsControlJustReleased(1, silnikKey) then
-            DisableControlAction(0, 32, true)
-            local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
-            if vehicle ~= nil and vehicle ~= 0 and GetPedInVehicleSeat(vehicle, 0) then
-                SetVehicleEngineOn(vehicle, (not GetIsVehicleEngineRunning(vehicle)), false, true)
+        if IsControlJustPressed(0, 74) and not searchingKeys and IsPedInAnyVehicle(playerPed, false) then
+            local hasKeys = HasItem("carkeys")
+
+            if not hasKeys then
+				local vehicleHash = GetVehiclePedIsIn(playerPed, false)
+				if vehicle ~= 0 then
+					StartSearchingKeys()
+				end
             end
         end
-
     end
 end)
 
-RegisterCommand('kluczyki', function()
-    TriggerEvent('svn-carkeys:zamknijotworz')
+function StartSearchingKeys()
+	local gracz = PlayerPedId()
+	local furagracza = GetVehiclePedIsIn(gracz, false)
+	local plate = GetVehicleNumberPlateText(furagracza)
+	local vehicleHash = GetVehiclePedIsIn(playerPed, false)
+    searchingKeys = true
+		TriggerEvent("mythic_progbar:client:progress", {
+			name = "unique_action_name",
+			duration = 5000,
+			label = "Szukanie...",
+			useWhileDead = false,
+			canCancel = false,
+			controlDisables = {
+				disableMovement = true,
+				disableCarMovement = true,
+				disableMouse = false,
+				disableCombat = true,
+			}})
+			local odds = math.random(1, 100)
+
+			Citizen.Wait(5000)
+
+			if odds <= 5 then
+				local randomNotify = math.random(1, 3)
+
+				if randomNotify == 1 then
+					ESX.ShowNotification('Znalazłeś klucze na siedzeniu')
+					TriggerServerEvent('svn-addkeys2', plate)
+				elseif randomNotify == 2 then
+					ESX.ShowNotification('Znalazłeś klucze w schowku')
+					TriggerServerEvent('svn-addkeys2', plate)
+				else
+					ESX.ShowNotification('Znalazłeś klucze na desce rozdzielczej')
+					TriggerServerEvent('svn-addkeys2', plate)
+				end
+			else
+				local randomnotify2 = math.random(1, 4)
+				if randomnotify2 == 1 then
+					ESX.ShowNotification('Nie znalazłeś kluczy')
+				elseif randomnotify2 == 2 then
+					ESX.ShowNotification('Znalazłeś kluczyk pod siedzeniem lecz jest on połamany')
+				elseif randomnotify2 == 3 then
+					ESX.ShowNotification('Znalazłeś kluczyk w schowku lecz jest on od innego auta')
+				elseif randomnotify2 == 4 then
+					ESX.ShowNotification('Nie znalazłeś kluczy, natomiast znalazłeś porwany bankot 100$')
+				end
+			end
+
+			searchingKeys = false
+end
+
+function HasItem(itemName)
+    local playerPed = PlayerPedId()
+    local playerInventory = ESX.GetPlayerData().inventory
+
+    for i = 1, #playerInventory, 1 do
+        if playerInventory[i].name == itemName then
+            return true
+        end
+    end
+
+    return false
+end
+
+
+-- Silnik
+local silnikKey = 246 
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(10)
+		local graczyk = PlayerPedId()
+		local furagracza = GetVehiclePedIsIn(graczyk, false)
+		local silnikonczyoff
+		
+		if IsPedGettingIntoAVehicle(graczyk) then
+			silnikonczyoff = (GetIsVehicleEngineRunning(furagracza)) 
+			if not (silnikonczyoff) then
+				SetVehicleEngineOn(furagracza, false, true, true) 
+				DisableControlAction(2, 71, true)
+			end
+		end
+		
+		if IsPedInAnyVehicle(graczyk, false) and not IsEntityDead(graczyk) and (not GetIsVehicleEngineRunning(furagracza)) then
+			DisableControlAction(2, 71, true) 
+		end
+		
+		if (IsControlJustReleased(0, silnikKey) or IsDisabledControlJustReleased(0, silnikKey)) and furagracza ~= nil and furagracza ~= 0 and GetPedInVehicleSeat(furagracza, 0) then
+			local plate = GetVehicleNumberPlateText(furagracza)
+				TriggerServerEvent('svn-silnik', plate)
+					if not (silnikonczyoff) then
+						silnikonczyoff = true
+						DisableControlAction(2, 71, false)
+					else
+						silnikonczyoff = false 
+						DisableControlAction(2, 71, true)
+					end
+		end
+		
+		if IsPedInAnyVehicle(graczyk, false) and IsControlPressed(2, 75) and not IsEntityDead(graczyk) then
+			if (GetIsVehicleEngineRunning(furagracza)) then
+				Citizen.Wait(150)
+				SetVehicleEngineOn(furagracza, true, true, false) 
+				TaskLeaveVehicle(graczyk, furagracza, 0)
+			else
+				TaskLeaveVehicle(graczyk, furagracza, 0)
+			end
+		end
+    end
 end)
 
-RegisterKeyMapping('kluczyki', 'Zamknij / Otwórz pojazd', 'keyboard', 'U')
+RegisterNetEvent('svn-carkeys:silnik')
+AddEventHandler('svn-carkeys:silnik', function(ped)
+	local graczyk = PlayerPedId(ped)
+	local furagracza = GetVehiclePedIsIn(graczyk, false)
+    if furagracza ~= nil and furagracza ~= 0 and GetPedInVehicleSeat(furagracza, 0) then
+        SetVehicleEngineOn(furagracza, (not GetIsVehicleEngineRunning(furagracza)), true, true)
+		SetVehicleJetEngineOn(furagracza, true)
+		ESX.ShowNotification('Przekręcono kluczyki pojazdu o Nr. Rej. ' ..GetVehicleNumberPlateText(furagracza))
+	end
+end)
